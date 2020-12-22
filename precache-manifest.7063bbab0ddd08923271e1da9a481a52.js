842377ec63008bbc7c462daa40146a668b3c9624
Righthand5 <xyx1530966653@163.com>
2020-12-22 16:21:24 +0800

    deploy

---
+++ precache-manifest.7063bbab0ddd08923271e1da9a481a52.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8090afc0094f637c5cd6",
+    "url": "/money-3/css/app.eda75f24.css"
+  },
+  {
+    "revision": "34dbda5986b7a279bfa7278906abef5d",
+    "url": "/money-3/index.html"
+  },
+  {
+    "revision": "8090afc0094f637c5cd6",
+    "url": "/money-3/js/app.cd15d555.js"
+  },
+  {
+    "revision": "04b16e8b34c0528ed918",
+    "url": "/money-3/js/chunk-vendors.bbc85d97.js"
+  },
+  {
+    "revision": "4e6544772c5d9b534714ec664a846fda",
+    "url": "/money-3/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-3/robots.txt"
+  },
+  {
+    "revision": "322f74013b2adba7c17d1669c760f6d5",
+    "url": "/money-3/website.png"
+  }
+]);
